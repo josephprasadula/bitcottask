0eb8c95a3eaa9b6d59dcdc2ad9454ab377367aaf
josephprasadula <josephprasadula@gmail.com>
2023-01-04 00:50:08 +0530

    added swr

--- context/context.js
+++ context/context.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import useSWR from 'swr';
 
 export const DataContext = React.createContext();
 
@@ -8,37 +9,35 @@ export function useData() {
 }
 export default function DataProvider({ children }) {
     const Router = useRouter();
-    const [data, setData] = useState();
-    useEffect(() => {
-        // getData()
-        // .then(data => {
-        //     setData(data);
-        //     console.log('data', data)
-        // })
-        // .catch(error => {
-        //     console.error(error);
-        //     // Router.push('/error');
-        // });
-    }, [])
-    // const getData = () => {
-    //     return new Promise((resolve, reject) => {
-    //         const xhr = new XMLHttpRequest();
-    //         xhr.open('GET', '../src/sample.json', true);
-    //         xhr.onload = function () {
-    //             if (this.status === 200) {
-    //                 resolve(JSON.parse(this.responseText));
-    //             } else {
-    //                 reject(new Error(`Failed to load data: ${this.status}`));
-    //             }
-    //         };
-    //         xhr.onerror = function () {
-    //             reject(new Error('Failed to load data'));
-    //         };
-    //         xhr.send();
-    //     });
-    // };
+    const [totalData, setTotalData] = useState();
+    const [loading, setLoading] = useState(false);
+    const [errorPage, setErrorPage] = useState(false)
+    const fetcher = (url) => fetch(url).then((res) => res.json());
+    const getData = (url) => {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.onload = function () {
+                if (this.status === 200) {
+                    resolve(JSON.parse(this.responseText));
+                } else {
+                    reject(new Error(`Failed to load data: ${this.status}`));
+                }
+            };
+            xhr.onerror = function () {
+                reject(new Error('Failed to load data'));
+            };
+            xhr.send();
+        });
+    };
+    const { data, error } = useSWR('/api/staticdata', getData);
+    if (error) return <div>Failed to load</div>;
+    if (!data) return <div>Loading...</div>;
+    if (data) console.log(data?.length)
 
-    const value = {}
+    const value = {
+        data
+    }
     return (
         <DataContext.Provider value={value}>
             {children}
