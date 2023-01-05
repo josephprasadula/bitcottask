715b15a5edb49cb7a03cee8aee7bd66e605e7931
josephprasadula <josephprasadula@gmail.com>
2023-01-06 00:38:48 +0530

    small change to config file

--- next.config.js
+++ next.config.js
@@ -12,9 +12,18 @@ const nextConfig = {
     unoptimized: true,
   },
   distDir: 'dist',
-  exportPathMap: () => ({
-    '/': { page: '/index' },
-  }),
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+    }
+  },
 }
 
 module.exports = nextConfig
